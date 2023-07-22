c90ac2c055a1bd6df4bf8ed51c362fcaed7e0f28
VagnerGiraldinoJunior <vgiraldinojr@gmail.com>
2023-07-22 10:59:43 -0300

    ok

---
+++ js.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('mensagem-sucesso').onclick = function(){
+    swal('Boa!', 'Deu tudo certo!', 'success')
+  };
+  
+  document.getElementById('mensagem-erro').onclick = function(){
+    swal('Oh no...', 'Algo deu errado!', 'error')
+  };
